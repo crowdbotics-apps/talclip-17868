18b287c8a8efdd85253e409303dca3c776ac4152
Crowdbotics <team@crowdbotics.com>
2020-06-05 23:00:09 +0000

    Update models

--- backend/talclip_17868/wsgi.py
+++ backend/talclip_17868/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talclip_17868.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "talclip_17868.settings")
 
 application = get_wsgi_application()
